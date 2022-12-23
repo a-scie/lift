732a4659fc8c89067aa7e5f70124630f43ccff1e
John Sirois <john.sirois@gmail.com>
2022-12-23 13:00:43 -0800

    More basic structure.

---
+++ science/__main__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Science project contributors.
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import sys
+from typing import NoReturn
+
+
+def main() -> NoReturn:
+    print("Science!")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
