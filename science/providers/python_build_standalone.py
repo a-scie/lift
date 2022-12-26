ef1dcdc5613bf4984fedce5e6951ac5a1866a90a
John Sirois <john.sirois@gmail.com>
2022-12-26 11:55:07 -0800

    Support inexact Python version.

--- science/providers/python_build_standalone.py
+++ science/providers/python_build_standalone.py
@@ -4,7 +4,10 @@
 from __future__ import annotations
 
 import re
+import sys
 from dataclasses import dataclass
+
+from packaging.version import Version
 
 from science.fetcher import fetch_json, fetch_text
 from science.model import Digest, Distribution, FileType, Provider
@@ -15,7 +18,7 @@ from science.platform import Platform
 class FingerprintedAsset:
     url: str
     digest: Digest
-    version: str
+    version: Version
     target_triple: str
     file_type: FileType
 
@@ -32,7 +35,7 @@ class Asset:
         return FingerprintedAsset(
             url=self.url,
             digest=Digest(size=self.size, fingerprint=fingerprint),
-            version=self.version,
+            version=Version(self.version),
             target_triple=self.target_triple,
             file_type=FileType.for_extension(self.extension),
         )
@@ -43,20 +46,28 @@ class PBS(Provider):
     @classmethod
     def create(cls, **kwargs) -> PBS:
         api_url = "https://api.github.com/repos/indygreg/python-build-standalone/releases"
-        release = kwargs["release"]
-        release_url = f"{api_url}/tags/{release}"
+        release_url = (
+            f"{api_url}/tags/{release}"
+            if (release := kwargs.get("release"))
+            else f"{api_url}/latest"
+        )
         release_data = fetch_json(release_url)
 
-        version = kwargs["version"]
-        flavor = kwargs["flavor"]
+        release = release_data["name"]
+        version = Version(kwargs["version"])
+        flavor = kwargs.get("flavor", "install_only")
 
         # Names are like:
         #  cpython-3.9.16+20221220-x86_64_v3-unknown-linux-musl-install_only.tar.gz
         name_re = re.compile(
-            rf"^cpython-(?P<exact_version>{re.escape(version)}\.\d+)"
+            rf"^cpython-(?P<exact_version>{re.escape(str(version))}(?:\.\d+)*)"
             rf"\+{re.escape(release)}-(?P<target_triple>.+)-{re.escape(flavor)}\.(?P<extension>.+)$"
         )
 
+        # N.B.: There are 3 types of files in PBS releases:
+        # 1. The release archive.
+        # 2. The release archive .sah256 file with its individual checksum.
+        # 3. The SHA256SUMS file with all the release archive checksums.
         sha256sums_url: str | None = None
         asset_mapping = {}
         for asset in release_data["assets"]:
@@ -93,12 +104,18 @@ class PBS(Provider):
             fingerprint = sha256sums[name]
             fingerprinted_assets.append(asset.with_fingerprint(fingerprint))
 
+        if not fingerprinted_assets:
+            raise ValueError(
+                f"No released assets found for release {release} Python {version} of flavor "
+                f"{flavor}."
+            )
+
         return cls(
             release=release, version=version, flavor=flavor, assets=tuple(fingerprinted_assets)
         )
 
     release: str
-    version: str
+    version: Version
     flavor: str
     assets: tuple[FingerprintedAsset, ...]
 
