0213b215433e434de255325f8754a2da090c7a3f
John Sirois <john.sirois@gmail.com>
2022-12-26 15:48:55 -0800

    Missed refactor: rename tests.

--- tests/test_a_scie.py
+++ tests/test_a_scie.py
@@ -8,13 +8,13 @@ from _pytest.tmpdir import TempPathFactory
 from science import a_scie
 
 
-def test_latest(tmp_path_factory: TempPathFactory) -> None:
+def test_ptex_latest(tmp_path_factory: TempPathFactory) -> None:
     dest_dir = tmp_path_factory.mktemp("staging")
     latest = a_scie.ptex(dest_dir=dest_dir)
     subprocess.run(args=[str(dest_dir / latest.name), "-V"], check=True)
 
 
-def test_version(tmp_path_factory: TempPathFactory) -> None:
+def test_ptex_version(tmp_path_factory: TempPathFactory) -> None:
     dest_dir = tmp_path_factory.mktemp("staging")
     latest = a_scie.ptex(dest_dir=dest_dir, version="0.6.0")
     assert (
