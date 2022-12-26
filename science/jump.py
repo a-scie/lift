8a61c54a8f0e7c51a030cc66797df004ba7bf11e
John Sirois <john.sirois@gmail.com>
2022-12-26 15:34:06 -0800

    TTL scie-jump and ptex latest as well.

--- science/jump.py
+++ science/jump.py
@@ -1,6 +1,7 @@
 # Copyright 2022 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from datetime import timedelta
 from pathlib import Path
 
 from science.fetcher import fetch_and_verify
@@ -10,5 +11,12 @@ from science.platform import Platform
 def load(platform: Platform = Platform.current(), version: str | None = None) -> Path:
     binary_name = platform.qualified_binary_name("scie-jump")
     base_url = "https://github.com/a-scie/jump/releases"
-    version_path = f"download/v{version}" if version else "latest/download"
-    return fetch_and_verify(url=f"{base_url}/{version_path}/{binary_name}", executable=True)
+    if version:
+        version_path = f"download/v{version}"
+        ttl = None
+    else:
+        version_path = "latest/download"
+        ttl = timedelta(days=5)
+    return fetch_and_verify(
+        url=f"{base_url}/{version_path}/{binary_name}", executable=True, ttl=ttl
+    )
--- science/ptex.py
+++ science/ptex.py
@@ -1,6 +1,7 @@
 # Copyright 2022 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from datetime import timedelta
 from pathlib import Path
 
 from science.fetcher import fetch_and_verify
@@ -13,7 +14,14 @@ def load(
 ) -> File:
     binary_name = platform.qualified_binary_name("ptex")
     base_url = "https://github.com/a-scie/ptex/releases"
-    version_path = f"download/v{version}" if version else "latest/download"
-    ptex_path = fetch_and_verify(url=f"{base_url}/{version_path}/{binary_name}", executable=True)
+    if version:
+        version_path = f"download/v{version}"
+        ttl = None
+    else:
+        version_path = "latest/download"
+        ttl = timedelta(days=5)
+    ptex_path = fetch_and_verify(
+        url=f"{base_url}/{version_path}/{binary_name}", executable=True, ttl=ttl
+    )
     (dest_dir / binary_name).symlink_to(ptex_path)
     return File(name=binary_name, key="ptex", is_executable=True)
