07114a9d0c262bcd2fe3cff7b4a40ba8a41a30af
John Sirois <john.sirois@gmail.com>
2023-01-25 09:53:54 -0800

    Fix ws.

--- science/frozendict.py
+++ science/frozendict.py
@@ -1,5 +1,6 @@
 # Copyright 2023 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 import copy
 from typing import Generic, Iterator, Mapping, TypeVar
 
