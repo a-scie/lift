7a48b2ebeca024e2bfcb6bd0721a011711b6be5b
John Sirois <john.sirois@gmail.com>
2023-03-24 07:18:54 -0700

    A less ridiculous config parse test.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,18 +1,25 @@
 # Copyright 2022 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import pprint
-import sys
 from pathlib import Path
 
 from science.config import parse_config_file
+from science.model import Identifier
 from science.platform import Platform
 
 
 def test_parse(build_root: Path) -> None:
     app = parse_config_file(build_root / "science.toml")
-    pprint.pp(app, stream=sys.stderr, indent=2)
-    for interpreter in app.interpreters:
-        pprint.pp(
-            interpreter.provider.distribution(Platform.current()), stream=sys.stderr, indent=2
-        )
+    interpreters = list(app.interpreters)
+    assert 1 == len(interpreters), "Expected science to ship on a single fixed interpreter."
+
+    interpreter = interpreters[0]
+    assert interpreter.lazy, "Expected science to ship as a gouged-out binary."
+
+    distribution = interpreter.provider.distribution(Platform.current())
+    assert (
+        distribution is not None
+    ), "Expected a Python interpreter distribution to be available for each platform tests run on."
+    assert (
+        Identifier.parse("python") in distribution.placeholders
+    ), "Expected the Python interpreter to expose a 'python' placeholder for its `python` binary."
