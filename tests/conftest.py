34eb6f3671aa8cd1865959a76e64660b33059109
John Sirois <john.sirois@gmail.com>
2023-04-22 14:39:29 -0700

    Make it clear when a test must be run via nox.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 # Copyright 2022 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 import os
 from pathlib import Path
 
@@ -8,9 +9,15 @@ import pytest
 
 @pytest.fixture(scope="session")
 def build_root() -> Path:
-    return Path(os.environ["BUILD_ROOT"])
+    try:
+        return Path(os.environ["BUILD_ROOT"])
+    except KeyError:
+        pytest.fail("Tests must be run via `nox` or `nox -etest`.")
 
 
 @pytest.fixture(scope="session")
 def science_pyz() -> Path:
-    return Path(os.environ["SCIENCE_TEST_PYZ_PATH"])
+    try:
+        return Path(os.environ["SCIENCE_TEST_PYZ_PATH"])
+    except KeyError:
+        pytest.fail("Test must be run via `nox` or `nox -etest`.")
