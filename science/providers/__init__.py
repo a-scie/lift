5488614e25b0b19000b871260cd8f3f6eb545a30
John Sirois <john.sirois@gmail.com>
2023-05-31 23:54:17 -0700

    Add support for listing installed providers.
    Also fix up the plugin provider logic.

--- science/providers/__init__.py
+++ science/providers/__init__.py
@@ -1,11 +1,16 @@
 # Copyright 2022 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import dataclasses
 import importlib.metadata
+import inspect
 import logging
-from typing import Iterator, Mapping
+import os
+import sys
+from dataclasses import dataclass
+from functools import cached_property
+from typing import Iterator
 
-from science.frozendict import FrozenDict
 from science.model import Provider
 
 from ..errors import InputError
@@ -14,24 +19,49 @@ from .python_build_standalone import PythonBuildStandalone
 _BUILTIN_PROVIDER_TYPES = (PythonBuildStandalone,)
 
 
-def _builtin_names(provider_type: type[Provider]) -> Iterator[str]:
-    # Name builtins as `science.providers.<type name>`, e.g.:
-    # `science.providers.PythonBuildStandalone`.
-    yield provider_type.__name__
-    yield f"{__name__}.{provider_type.__name__}"
+@dataclass(frozen=True)
+class ProviderInfo:
+    type: type[Provider]
+    source: str
+    short_name: str | None = None
+
+    @cached_property
+    def fully_qualified_name(self) -> str:
+        return f"{self.type.__module__}.{self.type.__qualname__}"
+
+    @cached_property
+    def summary(self) -> str | None:
+        if doc := inspect.getdoc(self.type):
+            return inspect.cleandoc(doc).splitlines()[0].strip()
+        return None
+
+    @cached_property
+    def description(self) -> str | None:
+        if doc := inspect.getdoc(self.type):
+            if desc := os.linesep.join(inspect.cleandoc(doc).splitlines()[1:]).strip():
+                return desc
+        return None
 
 
-def _builtin() -> Mapping[str, type[Provider]]:
-    return {
-        name: builtin_provider_type
-        for builtin_provider_type in _BUILTIN_PROVIDER_TYPES
-        for name in _builtin_names(builtin_provider_type)
-    }
+def _iter_builtin_providers() -> Iterator[ProviderInfo]:
+    for builtin_provider_type in _BUILTIN_PROVIDER_TYPES:
+        yield ProviderInfo(
+            type=builtin_provider_type,
+            source="builtin",
+            short_name=builtin_provider_type.__qualname__,
+        )
 
 
-def _plugins() -> Mapping[str, tuple[str, type[Provider]]]:
-    plugins = {name: ("builtins", provider_type) for name, provider_type in _builtin().items()}
-    for entry_point in importlib.metadata.entry_points().select(group="science.providers"):
+def _iter_providers() -> Iterator[ProviderInfo]:
+    providers_by_short_name = dict[str, ProviderInfo]()
+    for provider_info in _iter_builtin_providers():
+        yield provider_info
+        if provider_info.short_name:
+            providers_by_short_name[provider_info.short_name] = provider_info
+
+    for entry_point in importlib.metadata.entry_points().select(
+        group="science.providers_by_short_name"
+    ):
         provider_type = entry_point.load()
         source = (
             f"{entry_point.dist.name} {entry_point.dist.version}"
@@ -45,25 +75,33 @@ def _plugins() -> Mapping[str, tuple[str, type[Provider]]]:
                 f"{source} which does not."
             )
 
-        plugin_entry = source, provider_type
-        plugins[provider_type.__qualname__] = plugin_entry
-        if existing_entry := plugins[entry_point.name]:
-            provenance, existing_provider_type = existing_entry
+        provider_info = ProviderInfo(type=provider_type, source=source)
+        if existing_entry := providers_by_short_name.get(entry_point.name):
             logging.warning(
                 f"The Provider {provider_type.__qualname__} found in {source} has a short name of"
                 f"{entry_point.name!r} that conflicts with Provider "
-                f"{existing_provider_type.__qualname__} provided by {provenance}. Not registering "
-                f"{provider_type.__qualname__} under a that short name"
+                f"{existing_entry.type.__qualname__} provided by {existing_entry.source}. Not "
+                f"registering {provider_type.__qualname__} under a that short name"
             )
         else:
-            plugins[entry_point.name] = provider_type
-    return plugins
+            provider_info = dataclasses.replace(provider_info, short_name=entry_point.name)
+            providers_by_short_name[entry_point.name] = provider_info
+        yield provider_info
 
 
-_PROVIDERS = FrozenDict(
-    {name: provider_type for name, (_source, provider_type) in _plugins().items()}
+ALL_PROVIDERS = tuple[ProviderInfo, ...](
+    sorted(
+        _iter_providers(),
+        key=lambda provider_info: (
+            provider_info.short_name or chr(sys.maxunicode),
+            provider_info.fully_qualified_name,
+        ),
+    )
 )
 
 
 def get_provider(name: str) -> type[Provider] | None:
-    return _PROVIDERS.get(name)
+    for provider_info in ALL_PROVIDERS:
+        if name in (provider_info.short_name, provider_info.fully_qualified_name):
+            return provider_info.type
+    return None
