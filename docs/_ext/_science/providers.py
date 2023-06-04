76c4320b3def84f0a8353e66849709ce79534101
John Sirois <john.sirois@gmail.com>
2023-06-03 23:44:57 -0700

    Add sections for config params.

--- docs/_ext/_science/providers.py
+++ docs/_ext/_science/providers.py
@@ -1,9 +1,10 @@
 # Copyright 2023 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import itertools
+import os
+from dataclasses import dataclass
 from functools import cached_property
-from typing import Any, Iterable, Iterator
+from typing import Any, Iterable, Self
 
 from docutils import nodes
 from docutils.parsers.rst import Directive
@@ -17,6 +18,34 @@ from science import providers
 from science.providers import ProviderInfo
 
 
+@dataclass
+class Section:
+    @classmethod
+    def create(cls, *, title: str, name: str | None = None) -> Self:
+        identifier = name or title
+        section = nodes.section(
+            "",
+            nodes.title(text=title),
+            ids=[nodes.make_id(identifier)],
+            names=[nodes.fully_normalize_name(identifier)],
+        )
+        return cls(identifier=identifier, node=section)
+
+    identifier: str
+    node: nodes.Element
+
+    def append(self, child: nodes.Node) -> None:
+        self.node.append(child)
+
+    def extend(self, children: Iterable[nodes.Node]) -> None:
+        self.node.extend(children)
+
+    def create_subsection(self, *, title: str, name: str | None = None) -> Self:
+        subsection = self.create(title=title, name=f"{self.identifier}.{name or title}")
+        self.append(subsection.node)
+        return subsection
+
+
 class _Providers(Directive):
     @cached_property
     def _markdown_parser(self) -> MarkdownIt:
@@ -25,49 +54,33 @@ class _Providers(Directive):
     def _parse_markdown(self, text: str) -> Iterable[nodes.Node]:
         return self._markdown_parser.render(text).children
 
-    def _make_nodes(self, provider_info: ProviderInfo) -> Iterator[nodes.Node]:
+    def _make_node(self, provider_info: ProviderInfo) -> nodes.Node:
         assert (
             provider_info.short_name
         ), "Expected all built-in providers to have short names defined."
-        short_name = provider_info.short_name
-        yield nodes.section(
-            "",
-            nodes.title(text=short_name),
-            ids=[nodes.make_id(short_name)],
-            names=[nodes.fully_normalize_name(short_name)],
-        )
-
-        yield nodes.paragraph()
-        class_line = nodes.line()
-        class_line.extend(
-            self._parse_markdown(f"**class**: `{provider_info.fully_qualified_name}`")
-        )
-        yield class_line
+        provider_section = Section.create(title=provider_info.short_name)
 
         if provider_info.summary:
-            yield nodes.paragraph()
-            description = f"\n\n{provider_info.description}" if provider_info.description else ""
-            yield from self._parse_markdown(f"""**{provider_info.summary}**{description}""")
+            description = f"**{provider_info.summary}**"
+            if provider_info.description:
+                description = os.linesep.join((description, "", provider_info.description))
+            provider_section.extend(self._parse_markdown(description))
 
         for field_info in provider_info.iter_config_fields():
-            yield nodes.paragraph()
-            field_line = nodes.line()
-            default = f" [*default: {field_info.default!r}*]" if field_info.has_default else ""
-            field_line.extend(
-                self._parse_markdown(f"**{field_info.name}**: `{field_info.type}`{default}")
-            )
-            yield field_line
+            field_section = provider_section.create_subsection(title=field_info.name)
+
+            default = f" [default: `{field_info.default!r}`]" if field_info.has_default else ""
+            field_section.extend(self._parse_markdown(f"*type: `{field_info.type}`{default}*"))
 
             if field_info.help:
-                yield from self._parse_markdown(field_info.help)
+                field_section.extend(self._parse_markdown(field_info.help))
+
+        return provider_section.node
 
     def run(self) -> list[nodes.Node]:
-        return list(
-            itertools.chain.from_iterable(
-                self._make_nodes(provider_info)
-                for provider_info in providers.iter_builtin_providers()
-            )
-        )
+        return [
+            self._make_node(provider_info) for provider_info in providers.iter_builtin_providers()
+        ]
 
 
 def setup(app: application.Sphinx) -> dict[str, Any]:
