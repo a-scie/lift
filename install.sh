460a627a120e7c184bcfb1f117e84562ccb0ef40
Kris Wilson <kw@onoku.com>
2024-09-05 05:11:07 -0700

    Finish hoist to get-pants.sh mode.

--- install.sh
+++ install.sh
@@ -28,15 +28,149 @@ function warn() {
 
 function ensure_cmd() {
   local cmd="$1"
-  command -v "$cmd" > /dev/null || die "This script requires the ${cmd} binary to be on the PATH."
+  command -v "$cmd" > /dev/null || die "This script requires the ${cmd} binary to be on \$PATH."
 }
 
-GITHUB_DOWNLOAD_BASE="https://github.com/a-scie/lift/releases/latest/download"
+_GC=()
+
+ensure_cmd rm
+function gc() {
+  if (($# > 0)); then
+    _GC+=("$@")
+  else
+    # Check $_GC validity to avoid "unbound variable" warnings if gc w/ arguments is never called.
+    if ! [ ${#_GC[@]} -eq 0 ]; then
+      rm -rf "${_GC[@]}"
+    fi
+  fi
+}
+
+trap gc EXIT
+
+ensure_cmd uname
+function determine_os() {
+  local os
+
+  os="$(uname -s)"
+  if [[ "${os}" =~ [Ll]inux ]]; then
+    echo linux
+  elif [[ "${os}" =~ [Dd]arwin ]]; then
+    echo macos
+  elif [[ "${os}" =~ [Ww]in|[Mm][Ii][Nn][Gg] ]]; then
+    # Powershell reports something like: Windows_NT
+    # Git bash reports something like: MINGW64_NT-10.0-22621
+    echo windows
+  else
+    die "Science is not supported on this OS (${os}). Please reach out at https://github.com/a-scie/lift/issues for help."
+  fi
+}
+
+OS="$(determine_os)"
+
+ensure_cmd basename
+ensure_cmd $([[ "${OS}" == "windows" ]] && echo "pwsh" || echo "curl")
+function fetch() {
+  local url="$1"
+  local dest_dir="$2"
+
+  local dest
+  dest="${dest_dir}/$(basename "${url}")"
+
+  if [[ "${OS}" == "windows" ]]; then
+    pwsh -c "Invoke-WebRequest -OutFile ${dest} -Uri ${url}"
+  else
+    curl --proto '=https' --tlsv1.2 -sSfL -o "${dest}" "${url}"
+  fi
+}
+
+ensure_cmd $([[ "${OS}" == "macos" ]] && echo "shasum" || echo "sha256sum")
+function sha256() {
+  if [[ "${OS}" == "macos" ]]; then
+    shasum --algorithm 256 "$@"
+  else
+    sha256sum "$@"
+  fi
+}
+
+ensure_cmd mktemp
+ensure_cmd install
+function install_from_url() {
+  local url="$1"
+  local dest="$2"
+
+  local workdir
+  workdir="$(mktemp -d)"
+  gc "${workdir}"
+
+  fetch "${url}.sha256" "${workdir}"
+  fetch "${url}" "${workdir}"
+  (
+    cd "${workdir}"
+    sha256 -c --status ./*.sha256 ||
+      die "Download from ${url} did not match the fingerprint at ${url}.sha256"
+  )
+  rm "${workdir}/"*.sha256
+  if [[ "${OS}" == "macos" ]]; then
+    mkdir -p "$(dirname "${dest}")"
+    install -m 755 "${workdir}/"* "${dest}"
+  else
+    install -D -m 755 "${workdir}/"* "${dest}"
+  fi
+}
+
+ensure_cmd cat
+function usage() {
+  cat << __EOF__
+Usage: $0
+
+Installs the \`science\` binary.
+
+-h | --help: Print this help message.
+
+-d | --bin-dir:
+  The directory to install the science binary in, "~/.local/bin" by default.
+
+-b | --base-name:
+  The name to use for the science binary, "science" by default.
+
+-V | --version:
+  The version of the science binary to install, the latest version by default.
+  The available versions can be seen at:
+    https://github.com/a-scie/lift/releases
+
+__EOF__
+}
+
 INSTALL_PREFIX="${HOME}/.local/bin"
 INSTALL_FILE="science"
-INSTALL_DEST="${INSTALL_PREFIX}/${INSTALL_FILE}"
+VERSION="latest/download"
 
-ensure_cmd arch
+# Parse arguments.
+while (($# > 0)); do
+  case "$1" in
+    --help | -h)
+      usage
+      exit 0
+      ;;
+    --bin-dir | -d)
+      INSTALL_PREFIX="$2"
+      shift
+      ;;
+    --base-name | -b)
+      INSTALL_FILE="$2"
+      shift
+      ;;
+    --version | -V)
+      VERSION="download/v${2}"
+      shift
+      ;;
+    *)
+      usage
+      die "Unexpected argument: ${1}\n"
+      ;;
+  esac
+  shift
+done
 
 # Map platform architecture to released binary architecture.
 READ_ARCH="$(arch)"
@@ -48,32 +182,18 @@ case "$READ_ARCH" in
   *)        echo "unknown arch: $(READ_ARCH)"; exit 1 ;;
 esac
 
-# Map OS name to released binary OS names and platform-specific SHA checker invocation.
-case "$OSTYPE" in
-  darwin*)  OS="macos" SHASUM_TOOL="shasum" SHASUM_CMD="${SHASUM_TOOL} -a 256 -c -" ;;
-  linux*)   OS="linux" SHASUM_TOOL="sha256sum" SHASUM_CMD="${SHASUM_TOOL} -c -" ;;
-  msys*)    OS="windows" SHASUM_TOOL="sha256sum" SHASUM_CMD="${SHASUM_TOOL} -c -" ;;
-  *)        echo "unsupported platform: ${OSTYPE}, please download manually from https://github.com/a-scie/lift/releases/"; exit 1 ;;
-esac
 
-ensure_cmd "${SHASUM_TOOL}"
-ensure_cmd curl
-
+GITHUB_DOWNLOAD_BASE="https://github.com/a-scie/lift/releases/${VERSION}"
+INSTALL_DEST="${INSTALL_PREFIX}/${INSTALL_FILE}"
 DL_FILE="science-fat-${OS}-${ARCH}"
 DL_URL="${GITHUB_DOWNLOAD_BASE}/${DL_FILE}"
-SHA_URL="${DL_URL}.sha256"
 
 green "Download URL is: ${DL_URL}"
-green "Checksum URL is: ${SHA_URL}"
 
 log "Ensuring ${INSTALL_PREFIX}"
 mkdir -p "${INSTALL_PREFIX}"
 
-curl -fLO --progress-bar "$DL_URL" && \
-  curl -fL --progress-bar "$SHA_URL" | ${SHASUM_CMD} && \
-  chmod +x "$DL_FILE" && \
-  mv -v "$DL_FILE" "${INSTALL_DEST}"
-
+install_from_url "${DL_URL}" "${INSTALL_DEST}"
 green "Installed ${DL_FILE} to ${INSTALL_DEST}"
 
 # Warn if the install prefix is not on $PATH.
