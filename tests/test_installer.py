bba3a4a39d4e5e0c5259a4b6523b1122bf3a2585
Kris Wilson <kw@onoku.com>
2024-09-05 17:00:54 -0700

    Modernize tests for 3.12.

--- tests/test_installer.py
+++ tests/test_installer.py
@@ -13,36 +13,47 @@ def installer(build_root: Path) -> Path:
     return build_root / "install.sh"
 
 
+def run_captured(cmd: list):
+    return subprocess.run(cmd, capture_output=True, text=True)
+
+
 def test_installer_help(installer: Path):
     """Validates -h|--help in the installer."""
-    assert "--help" in subprocess.check_output([installer, "-h"]).decode("utf-8")
-    assert "--help" in subprocess.check_output([installer, "--help"]).decode("utf-8")
+    for tested_flag in ("-h", "--help"):
+        assert (result := run_captured([installer, tested_flag])).returncode == 0
+        assert "--help" in result.stdout, "Expected '--help' in tool output"
 
 
 def test_installer_fetch_latest(tmp_path_factory: TempPathFactory, installer: Path):
     """Invokes install.sh to fetch the latest science release binary, then invokes it."""
     test_dir = tmp_path_factory.mktemp("install-test-default")
-    assert "success" in subprocess.check_output(
-        [installer, "-d", f"{test_dir}/bin"],
-        stderr=subprocess.STDOUT,
-    ).decode("utf-8")
-    assert subprocess.check_output([f"{test_dir}/bin/science", "-V"]).strip()
+
+    assert (result := run_captured([installer, "-d", f"{test_dir}/bin"])).returncode == 0
+    assert "success" in result.stderr, "Expected 'success' in tool stderr logging"
+
+    assert (result := run_captured([f"{test_dir}/bin/science", "-V"])).returncode == 0
+    assert result.stdout.strip(), "Expected version output in tool stdout"
 
 
 def test_installer_fetch_argtest(tmp_path_factory: TempPathFactory, installer: Path):
     """Exercises all the options in the installer."""
     test_dir = tmp_path_factory.mktemp("install-test")
-    output = subprocess.check_output(
-        [installer, "-V", "0.6.1", "-b", "science061", "-d", f"{test_dir}/bin"],
-        stderr=subprocess.STDOUT,
-    ).decode("utf-8")
-    assert "success" in output
+    test_ver = "0.6.1"
 
-    # Ensure missing $PATH entry warning.
-    assert f"WARNING: {test_dir}/bin is not detected on $PATH" in output
+    assert (
+        result := run_captured(
+            [installer, "-V", test_ver, "-b", f"science{test_ver}", "-d", f"{test_dir}/bin"]
+        )
+    ).returncode == 0
+    assert "success" in result.stderr, "Expected 'success' in tool stderr logging"
+
+    # Ensure missing $PATH entry warning (assumes our temp dir by nature is not on $PATH).
+    assert (
+        f"WARNING: {test_dir}/bin is not detected on $PATH" in result.stderr
+    ), "Expected missing $PATH entry warning"
 
     # Check expected versioned binary exists.
+    assert (result := run_captured([f"{test_dir}/bin/science{test_ver}", "-V"])).returncode == 0
     assert (
-        subprocess.check_output([f"{test_dir}/bin/science061", "-V"]).decode("utf-8").strip()
-        == "0.6.1"
-    )
+        result.stdout.strip() == test_ver
+    ), f"Expected version output in tool stdout to be {test_ver}"
