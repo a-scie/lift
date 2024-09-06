db9530015efc49c2ec6d5ba00185bb55b070e425
Kris Wilson <kw@onoku.com>
2024-09-05 20:11:45 -0700

    Workaround shebang on Windows.

--- tests/test_installer.py
+++ tests/test_installer.py
@@ -7,42 +7,45 @@ from pathlib import Path
 import pytest
 from _pytest.tmpdir import TempPathFactory
 
+from science.os import IS_WINDOWS
+
 
 @pytest.fixture(scope="module")
-def installer(build_root: Path) -> Path:
-    return build_root / "install.sh"
+def installer(build_root: Path) -> list:
+    installer = build_root / "install.sh"
+    return ["bash", installer] if IS_WINDOWS else [installer]
 
 
 def run_captured(cmd: list):
     return subprocess.run(cmd, capture_output=True, text=True)
 
 
-def test_installer_help(installer: Path):
+def test_installer_help(installer: list):
     """Validates -h|--help in the installer."""
     for tested_flag in ("-h", "--help"):
-        assert (result := run_captured([installer, tested_flag])).returncode == 0
+        assert (result := run_captured(installer + [tested_flag])).returncode == 0
         assert "--help" in result.stdout, "Expected '--help' in tool output"
 
 
-def test_installer_fetch_latest(tmp_path_factory: TempPathFactory, installer: Path):
+def test_installer_fetch_latest(tmp_path_factory: TempPathFactory, installer: list):
     """Invokes install.sh to fetch the latest science release binary, then invokes it."""
     test_dir = tmp_path_factory.mktemp("install-test-default")
 
-    assert (result := run_captured([installer, "-d", f"{test_dir}/bin"])).returncode == 0
+    assert (result := run_captured(installer + ["-d", f"{test_dir}/bin"])).returncode == 0
     assert "success" in result.stderr, "Expected 'success' in tool stderr logging"
 
     assert (result := run_captured([f"{test_dir}/bin/science", "-V"])).returncode == 0
     assert result.stdout.strip(), "Expected version output in tool stdout"
 
 
-def test_installer_fetch_argtest(tmp_path_factory: TempPathFactory, installer: Path):
+def test_installer_fetch_argtest(tmp_path_factory: TempPathFactory, installer: list):
     """Exercises all the options in the installer."""
     test_dir = tmp_path_factory.mktemp("install-test")
     test_ver = "0.6.1"
 
     assert (
         result := run_captured(
-            [installer, "-V", test_ver, "-b", f"science{test_ver}", "-d", f"{test_dir}/bin"]
+            installer + ["-V", test_ver, "-b", f"science{test_ver}", "-d", f"{test_dir}/bin"]
         )
     ).returncode == 0
     assert "success" in result.stderr, "Expected 'success' in tool stderr logging"
