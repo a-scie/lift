ac087cdc9f065df8a6f1b1e44d634febc6e4f38d
John Sirois <john.sirois@gmail.com>
2025-03-11 08:28:49 -0700

    Support scie name / file name collisions. (#145)
    Previously the scie was built in the same directory used to assemble its
    files which would lead to an error to write out the scie when its name
    matched one of those files.

--- science/fs.py
+++ science/fs.py
@@ -8,11 +8,18 @@ from contextlib import contextmanager
 from pathlib import Path
 from typing import Iterator
 
+from science.cache import science_cache
+
+_TMP_BASE_DIR = science_cache() / ".tmp"
+
 
 @contextmanager
-def temporary_directory(cleanup: bool) -> Iterator[Path]:
-    if cleanup:
-        with tempfile.TemporaryDirectory() as td:
-            yield Path(td)
-    else:
-        yield Path(tempfile.mkdtemp())
+def temporary_directory(prefix: str, delete: bool = True) -> Iterator[Path]:
+    _TMP_BASE_DIR.mkdir(parents=True, exist_ok=True)
+    with tempfile.TemporaryDirectory(
+        dir=_TMP_BASE_DIR,
+        prefix=prefix if prefix.endswith(".") else f"{prefix}.",
+        delete=delete,
+        ignore_cleanup_errors=True,
+    ) as td:
+        yield Path(td)
