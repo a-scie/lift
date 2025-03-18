2b9cce64f8a6446579fd778c934f3b9414f7ce6c
John Sirois <john.sirois@gmail.com>
2025-03-18 12:00:33 -0700

    Fix plumbing of env var based Bearer authentication.
    Fixes #127

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -1,13 +1,15 @@
 # Copyright 2024 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
-
+import base64
 import shutil
 from pathlib import Path
 
+import httpx
 from pytest import MonkeyPatch
+from pytest_httpx import HTTPXMock
 from testing import issue
 
-from science.fetcher import fetch_text
+from science.fetcher import fetch_json, fetch_text
 from science.model import Url
 
 
@@ -32,3 +34,40 @@ def test_netrc_directory(_, tmp_path: Path, monkeypatch: MonkeyPatch, cache_dir:
     monkeypatch.setenv("HOME", str(home_dir))
     (home_dir / ".netrc").mkdir(parents=True)
     assert_fetch()
+
+
+def assert_auth(
+    httpx_mock: HTTPXMock, cache_dir: Path, *, expected_authorization_header_value: str
+) -> None:
+    # N.B.: Ensure a fresh, un-cached fetch.
+    shutil.rmtree(cache_dir, ignore_errors=True)
+
+    def reflect_headers(request: httpx.Request) -> httpx.Response:
+        return httpx.Response(status_code=200, json=dict(request.headers.multi_items()))
+
+    httpx_mock.add_callback(reflect_headers)
+
+    headers = httpx.Headers(
+        fetch_json(
+            Url("https://api.github.com/repos/astral-sh/python-build-standalone/releases/latest")
+        )
+    )
+    assert expected_authorization_header_value == headers.get(
+        "Authorization"
+    ), f"Got headers: {headers}"
+
+
+def test_basic_auth(monkeypatch: MonkeyPatch, httpx_mock: HTTPXMock, cache_dir: Path) -> None:
+    monkeypatch.setenv("SCIENCE_AUTH_API_GITHUB_COM_BASIC_USER", "Arthur")
+    monkeypatch.setenv("SCIENCE_AUTH_API_GITHUB_COM_BASIC_PASS", "Dent")
+    assert_auth(
+        httpx_mock,
+        cache_dir,
+        expected_authorization_header_value=f"Basic {base64.b64encode(b'Arthur:Dent').decode()}",
+    )
+
+
+@issue(127, ignore=True)
+def test_bearer_auth(_, monkeypatch: MonkeyPatch, httpx_mock: HTTPXMock, cache_dir: Path) -> None:
+    monkeypatch.setenv("SCIENCE_AUTH_API_GITHUB_COM_BEARER", "Zaphod")
+    assert_auth(httpx_mock, cache_dir, expected_authorization_header_value="Bearer Zaphod")
