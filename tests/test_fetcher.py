d89a35570b898455ab7643b191bd830aa4b9a060
John Sirois <john.sirois@gmail.com>
2025-03-18 12:21:13 -0700

    Fix tests for CI where `SCIENCE_AUTH_*` is set.

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -1,6 +1,7 @@
 # Copyright 2024 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 import base64
+import os
 import shutil
 from pathlib import Path
 
@@ -37,10 +38,23 @@ def test_netrc_directory(_, tmp_path: Path, monkeypatch: MonkeyPatch, cache_dir:
 
 
 def assert_auth(
-    httpx_mock: HTTPXMock, cache_dir: Path, *, expected_authorization_header_value: str
+    monkeypatch: MonkeyPatch,
+    httpx_mock: HTTPXMock,
+    cache_dir: Path,
+    *,
+    expected_authorization_header_value: str,
+    **auth_env_vars: str,
 ) -> None:
     # N.B.: Ensure a fresh, un-cached fetch.
     shutil.rmtree(cache_dir, ignore_errors=True)
+
+    # N.B.: Ensure any ambient auth setup (like we have in CI) is ignored.
+    for key in os.environ:
+        if key.startswith("SCIENCE_AUTH_"):
+            monkeypatch.delenv(key)
+
+    for name, value in auth_env_vars.items():
+        monkeypatch.setenv(name, value)
 
     def reflect_headers(request: httpx.Request) -> httpx.Response:
         return httpx.Response(status_code=200, json=dict(request.headers.multi_items()))
@@ -57,19 +71,23 @@ def assert_auth(
     ), f"Got headers: {headers}"
 
 
-def test_basic_auth(httpx_mock: HTTPXMock, cache_dir: Path) -> None:
-    with MonkeyPatch.context() as monkeypatch:
-        monkeypatch.setenv("SCIENCE_AUTH_API_GITHUB_COM_BASIC_USER", "Arthur")
-        monkeypatch.setenv("SCIENCE_AUTH_API_GITHUB_COM_BASIC_PASS", "Dent")
-        assert_auth(
-            httpx_mock,
-            cache_dir,
-            expected_authorization_header_value=f"Basic {base64.b64encode(b'Arthur:Dent').decode()}",
-        )
+def test_basic_auth(monkeypatch: MonkeyPatch, httpx_mock: HTTPXMock, cache_dir: Path) -> None:
+    assert_auth(
+        monkeypatch,
+        httpx_mock,
+        cache_dir,
+        expected_authorization_header_value=f"Basic {base64.b64encode(b'Arthur:Dent').decode()}",
+        SCIENCE_AUTH_API_GITHUB_COM_BASIC_USER="Arthur",
+        SCIENCE_AUTH_API_GITHUB_COM_BASIC_PASS="Dent",
+    )
 
 
 @issue(127, ignore=True)
-def test_bearer_auth(_, httpx_mock: HTTPXMock, cache_dir: Path) -> None:
-    with MonkeyPatch.context() as monkeypatch:
-        monkeypatch.setenv("SCIENCE_AUTH_API_GITHUB_COM_BEARER", "Zaphod")
-        assert_auth(httpx_mock, cache_dir, expected_authorization_header_value="Bearer Zaphod")
+def test_bearer_auth(_, monkeypatch: MonkeyPatch, httpx_mock: HTTPXMock, cache_dir: Path) -> None:
+    assert_auth(
+        monkeypatch,
+        httpx_mock,
+        cache_dir,
+        expected_authorization_header_value="Bearer Zaphod",
+        SCIENCE_AUTH_API_GITHUB_COM_BEARER="Zaphod",
+    )
