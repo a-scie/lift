97391dda3c66819650648b672bae941c0ceddcb5
John Sirois <john.sirois@gmail.com>
2025-11-24 12:25:15 -0800

    Eliminate annotations __future__ import.
    No longer needed in 3.14.

--- science/commands/lift.py
+++ science/commands/lift.py
@@ -1,7 +1,6 @@
 # Copyright 2023 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import annotations
 
 import atexit
 import dataclasses
--- science/config.py
+++ science/config.py
@@ -1,7 +1,6 @@
 # Copyright 2022 Science project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import annotations
 
 import dataclasses
 import difflib
